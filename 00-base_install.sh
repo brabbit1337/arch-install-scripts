6a7b9322fc2e2f947eb82de4bb2debbbc90c4459
Györkös Imre <88290711+brabbit1337@users.noreply.github.com>
2023-10-26 19:57:43 +0200

    Add files via upload

---
+++ 00-base_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# BASE INSTALL
+sudo pacman -S --noconfirm --needed base-devel
+sudo pacman -S --noconfirm --needed git
+sudo pacman -S --noconfirm --needed nano
+
+# BASE XORG
+sudo pacman -S --noconfirm --needed xorg-server
+sudo pacman -S --noconfirm --needed xorg-xinit
+sudo pacman -S --noconfirm --needed libx11
+sudo pacman -S --noconfirm --needed libxinerama
+sudo pacman -S --noconfirm --needed libxft
+sudo pacman -S --noconfirm --needed webkit2gtk
