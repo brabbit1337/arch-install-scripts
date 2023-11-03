8b2c6dc862da769f4d22d2413a6c038d6a0c8f21
Györkös Imre <88290711+brabbit1337@users.noreply.github.com>
2023-11-03 11:37:34 +0100

    Update 04-aur_program_install.sh

--- 04-aur_program_install.sh
+++ 04-aur_program_install.sh
@@ -3,8 +3,6 @@ set -e
 
 paru -S --noconfirm --needed pamac-aur
 paru -S --noconfirm --needed noto-fonts-cjk
-paru -S --noconfirm --needed mangohud lib32-mangohud
-paru -S --noconfirm --needed goverlay
 paru -S --noconfirm --needed spotify
 paru -S --noconfirm --needed spotify-adblock
 paru -S --noconfirm --needed tamsyn-font
