a04cc42b476dce7d440f44d220c92d0950eecb37
Györkös Imre <88290711+brabbit1337@users.noreply.github.com>
2023-11-09 16:09:32 +0100

    Update 04-aur_program_install.sh

--- 04-aur_program_install.sh
+++ 04-aur_program_install.sh
@@ -7,7 +7,6 @@ paru -S --noconfirm --needed spotify
 paru -S --noconfirm --needed spotify-adblock
 paru -S --noconfirm --needed tamsyn-font
 paru -S --noconfirm --needed sublime-text-4
-paru -S --noconfirm --needed nordzy-cursors
 paru -S --noconfirm --needed oversteer
 paru -S --noconfirm --needed xpadneo-dkms
 paru -S --noconfirm --needed protonup-qt-bin
@@ -16,4 +15,3 @@ paru -S --noconfirm --needed betterdiscordctl
 paru -S --noconfirm --needed heroic-games-launcher
 paru -S --noconfirm --needed thorium-browser-bin
 paru -S --noconfirm --needed dxvk-bin
-paru -S --noconfirm --needed nordic-wallpapers
