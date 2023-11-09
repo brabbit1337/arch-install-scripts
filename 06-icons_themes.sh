6341773afe9039905ee055db9829725ca019f636
Györkös Imre <88290711+brabbit1337@users.noreply.github.com>
2023-11-09 16:09:50 +0100

    Update 06-icons_themes.sh

--- 06-icons_themes.sh
+++ 06-icons_themes.sh
@@ -5,3 +5,4 @@ paru -S --noconfirm --needed papirus-folders-nordic
 paru -S --noconfirm --needed nordic-theme
 paru -S --noconfirm --needed papirus-icon-theme
 paru -S --noconfirm --needed nordic-wallpaper
+paru -S --noconfirm --needed nordzy-cursors
